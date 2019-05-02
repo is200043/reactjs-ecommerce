e20cffee7225866d48c439aae63020ca775abb2f
Chonlakorn Punphopthaworn <chonlakorn.pun@gmail.com>
2019-05-02 22:43:38 +0700

    commit
    search

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Box, Grommet } from 'grommet';
+import { Box, Grommet, TextInput } from 'grommet';
 import AppBar from './components/AppBar';
 import ProductList from './components/ProductList';
 import './App.css';
 
 function App() {
+  const [value, setValue] = React.useState('');
+  const onBlur = () => {
+    console.log(value);
+  }
   return (
     <div className="App">
       <Box direction="column" fill>
         <AppBar />
         <Box direction="row" pad="medium" fill >
           <Box width="medium">
-            search
+            <Box direction="row" basis="medium" pad="small">
+              <TextInput
+                placeholder="Search here"
+                value={value}
+                onChange={event => setValue(event.target.value)}
+                onBlur={onBlur}
+              />
+            </Box>
           </Box>
           <Box flex>
-            <ProductList />
+            <ProductList search={value}/>
           </Box>
         </Box>
       </Box>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -10,6 +10,14 @@ class ProductList extends React.Component {
     componentDidMount() {
         this.fetchData();
     }
+    componentDidUpdate() {
+        if (this.props.search === "") {
+            this.fetchData();
+        } else {
+            console.log(this.props.search);
+            this.findData();
+        }
+    }
     fetchData = async () => {
         const res = await request.get('/products');
         const data = res.data.data.map(item => {
@@ -23,8 +31,23 @@ class ProductList extends React.Component {
         this.setState({ data: data });
         console.log(data);
     }
+    findData = async () => {
+        const res = await request.get('/products?filter=eq(name,' + this.props.search + ')');
+        const data = res.data.data.map(item => {
+            return {
+                name: item.name,
+                description: item.description,
+                image: 'https://via.placeholder.com/300x400.png',
+                price: item.meta.display_price.with_tax.formatted
+            }
+        })
+        this.setState({ data: data });
+        console.log(data);
+    }
+
     render() {
         const { data } = this.state;
+        const { search } = this.props;
         return (
             <Box direction='column' pad='small' fill >
                 <Box pad='small' background='light-3'>
